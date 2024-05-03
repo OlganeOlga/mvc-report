b6279bf5e8ef2447b2a24284ad887340c86201df
OlganeOlga <o_yegorovar@yahoo.com>
2024-05-03 12:58:56 +0200

    24/05/03 Add row with info to table 'book' works

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -20,23 +20,6 @@ class LibraryController extends AbstractController
         ]);
     }
 
-    #[Route('/library/create', name: 'create_book_get')]
-    public function readBook(
-        // ManagerRegistry $doctrine
-    ): Response {
-        // $entityManager = $doctrine->getManager();
-
-        // $book = new Book();
-
-        // $entityManager->persist($book);
-
-        // $entityManager->flush();
-
-        // $bookid = $book->getId();
-        // return $this->render('library/create.html.twig', ['bookid' => $bookid]);
-        return $this->render('library/create.html.twig');
-    }
-
     #[Route('/library/create1', name: 'library_create')]
     public function createBook1(
         ManagerRegistry $doctrine
@@ -58,30 +41,91 @@ class LibraryController extends AbstractController
 
         return new Response('Saved new book with id '.$book->getId());
     }
-
-    #[Route('/library/create', name: 'create_book_post', methods: ['POST'])]
-    public function createBook(
+    #[Route('/library/create', name: 'create_book', methods: ['GET', 'POST'])]
+    public function handleBookForm(
         ManagerRegistry $doctrine,
         Request $request,
         BookRepository $bookRepository
     ): Response {
         $entityManager = $doctrine->getManager();
 
-        $book = new Book();
+        // Check if the request is a POST request
+        if ($request->isMethod('POST')) {
+            // Process the form submission
+            // Create a new book object
+            $book = new Book();
+            $entityManager->persist($book);
 
-        //$bookid = $book->getId();
+            // Get book info from the request
+            $title = $request->request->get('title');
+            $isbn = $request->request->get('isbn');
+            $author = $request->request->get('author');
+            $cover = $request->request->get('cover');
 
-        // Update book properties with form data
-        $book->setTitle($request->request->get('title'));
-        $book->setIsbn($request->request->get('isbn'));
-        $book->setAuthor($request->request->get('author'));
-        $book->setCover($request->request->get('cover'));
+            $book->setTitle($title);
+            $book->setIsbn(intval($isbn));
+            $book->setAuthor($author);
+            $book->setCover($cover);
 
-        // Persist changes to the database
-        $entityManager->flush();
+            // Persist changes to the database
+            $entityManager->flush();
 
-        return $this->redirectToRoute('app_library');
-        // return $this->render('library/index.html.twig');
-        //return new Response('Saved new product with id '.$book->getId());
+            return $this->redirectToRoute('app_library');
+        }
+
+        // If the request is a GET request, render the form template
+        return $this->render('library/create.html.twig');
     }
+
+    // #[Route('/library/create', name: 'create_book_get')]
+    // public function readBook(
+    //     // ManagerRegistry $doctrine
+    // ): Response {
+    //     // $entityManager = $doctrine->getManager();
+
+    //     // $book = new Book();
+
+    //     // $entityManager->persist($book);
+
+    //     // $entityManager->flush();
+
+    //     // $bookid = $book->getId();
+    //     // return $this->render('library/create.html.twig', ['bookid' => $bookid]);
+    //     return $this->render('library/create.html.twig');
+    // }
+
+    // #[Route('/library/create', name: 'create_book_post', methods: ['POST'])]
+    // public function createBook(
+    //     ManagerRegistry $doctrine,
+    //     Request $request,
+    //     BookRepository $bookRepository
+    // ): Response {
+    //     $entityManager = $doctrine->getManager();
+
+    //     //crete new book object
+    //     $book = new Book();
+    //     $entityManager->persist($book);
+
+    //     $entityManager->flush();
+
+    //     // get boks info from request
+    //     $title = $request->request->get('title');
+    //     $isbn = $request->request->get('isbn');
+    //     $author = $request->request->get('author');
+    //     $cover = $request->request->get('cover');
+
+    //     $book->setTitle($title);
+    //     $book->setIsbn(intval($isbn));
+    //     $book->setAuthor($author);
+    //     $book->setCover($cover);
+
+    //     $entityManager->persist($book);
+
+    //     // Persist changes to the database
+    //     $entityManager->flush();
+
+    //     return $this->redirectToRoute('app_library');
+    //     // return $this->render('library/index.html.twig');
+    //     //return new Response('Saved new product with id '.$book->getId());
+    // }
 }
